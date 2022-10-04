1c00ba11e690e3a376bf253d191f15a289c8b8ff
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 03:36:16 +0300

    Do While Example

---
+++ EB-T7_DoWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            do
+            {
+
+                Console.WriteLine(a);
+                a++;
+
+
+            } while (a <= 10);
+
+            Console.WriteLine("\n");
+
+
+            int b = 1;
+            int toplam = 0;
+
+            do
+            {
+                toplam += b;
+                b++;
+
+
+            } while (b <= 10);
+
+            Console.WriteLine("1'den 10'a kadar olan sayıların Toplamı:" + toplam);
+            Console.ReadLine();
+
+
+        }
+    }
+}
